2ee04d69049ee21c709e80a074be1ffc23943433
percussionfloh <florian.moldaschl@web.de>
2025-11-27 17:56:15 +0100

    added vorhalte files, just an example - would be better if directly import data from krn scores, not vorhalte.yaml

---
+++ app/pages/vorhalte.vue
@@ -0,0 +1,9 @@
+<script setup>
+    // This is a placeholder file for the "Vorhalte" page.
+    // The actual content and logic for this page should be implemented here.
+
+</script>
+
+<template>
+	...
+</template>
