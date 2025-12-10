bb218981816a9661e4070dd77187edb53b4b673c
percussionfloh <florian.moldaschl@web.de>
2025-12-10 19:18:06 +0100

    create suspensions.vue

---
+++ app/pages/suspensions.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { t } = useI18n();
+
+useHead({
+    title: t('home'),
+});
+</script>
+
+<template>
+	<div class="h-[300px]">
+        <Chart :config="fbConfig" @chart-click="(chart, event) => chartClickHandler(viewFbMode, chart, event)" />
+    </div>
+</template>
