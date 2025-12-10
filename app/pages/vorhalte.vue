6737e19bf5f2bd4b30b012ee8b253b81917f5045
percussionfloh <florian.moldaschl@web.de>
2025-12-10 19:18:12 +0100

    changed vorhalte.vue to suspensions.vue

--- app/pages/vorhalte.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    // This is a placeholder file for the "Vorhalte" page.
-    // The actual content and logic for this page should be implemented here.
-
-</script>
-
-<template>
-	...
-</template>
