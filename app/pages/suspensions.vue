7b741d1c3c964e651aad7282815519a17e453f94
percussionfloh <florian.moldaschl@web.de>
2025-12-12 10:26:52 +0100

    create suspensions.vue

--- app/pages/suspensions.vue
+++ app/pages/suspensions.vue
@@ -1,10 +1,20 @@
 <script setup>
-const { t } = useI18n();
+import { computed } from 'vue'
+import Chart from '@/components/Chart.vue';
 
-useHead({
-    title: t('home'),
-});
+const fbConfig = computed(() => ({
+    type: 'bar',
+    data: {
+        datasets: [
+            {
+                label: viewFbMode.value === 'fb' ? t('fb') : t('exactIntervals'),
+                data: fbGroupedChords.value.map(i => ({ x: i[0], y: i[1] })),
+            }
+        ],
+    },
+}));
 </script>
+
 
 <template>
 	<div class="h-[300px]">
